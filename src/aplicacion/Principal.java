518ec53d3271b5b0c058604457bb1929d59f939d
Javier <javier.garciabermejolillo@usp.ceu.es>
2024-02-18 21:03:45 +0100

    codigo antes de compilar

---
+++ src/aplicacion/Principal.java
@@ -0,0 +1,11 @@
+package aplicacion;
+
+
+import mates.Funciones;
+
+
+public class Principal{
+    public static void main(String[] args){
+System.out.print("La suma de los números hasta el 10 es" + Funciones.suma(10));
+    }
+}
