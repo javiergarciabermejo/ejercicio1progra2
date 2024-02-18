316eb1fdfef6acec706c0143b36cfbe6b238ce88
Javier <javier.garciabermejolillo@usp.ceu.es>
2024-02-18 21:04:52 +0100

    codigo antes de compilar

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,11 +1,21 @@
 package aplicacion;
-
-
-import mates.Funciones;
+import operaciones.*;
 
 
 public class Principal{
+
+    
     public static void main(String[] args){
-System.out.print("La suma de los números hasta el 10 es" + Funciones.suma(10));
+
+
+
+        System.out.println("1) La suma de los 10 primeros números naturaleses: "+ Operaciones.sumaMemoria(0, 0, 0));
+        System.out.println("1.2) la suma de los 10 priemeros números naturaleses:" + Operaciones.sumaVersion2(0));
+    
     }
 }
+
+
+
+
+
